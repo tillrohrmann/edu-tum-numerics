108049f188565dc41d12660322ec0552b5d3b146
j.reifferscheid@googlemail.com <j.reifferscheid@googlemail.com@f8a14a00-d46b-11de-901b-a378632c6355>
2010-06-29 17:17:03 +0000

    aufgabe 1 von testat 2 angefangen

---
+++ trunk/testat2/cg/poisson_example.m
@@ -0,0 +1,30 @@
+%%
+%    Solve Ax = b, where A is the Poisson matrix and b = (1,...,1)T.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear all; close all;
+
+%%
+col = {'b*-','go-','r+-','cd--'};
+
+Nmax = 5;
+N = 4;
+%% hier wieder durch 1:4 ersetzen!
+for ii=1:1
+    m = zeros(Nmax,1);
+    N = 4;
+    for j=1:Nmax
+        A = gallery('poisson',N);
+        b = ones(N*N,1);
+        x0 = zeros(N*N,1);
+        [x,m(j)] = solvePCG(A,b,x0,1e-3,10000,ii);
+        N = 2*N;
+    end
+    plot(1:Nmax,m,col{ii},'LineWidth',3);
+    grid;
+    hold on;
+end
+
+legend('Jacobi','SGS','SSOR', 'NONE', 'Location', 'Best');
+
+hold off;
---
+++ trunk/testat2/cg/solvePCG.m
@@ -0,0 +1,51 @@
+function [ x, m ] = solvePCG( A, b, x0, tol, maxit, ptype )
+%SOLVEPCG Summary of this function goes here
+%   Detailed explanation goes here
+
+    m = 0;   
+    
+    switch ptype 
+        case 1
+            % Jacobi
+            % B = D^-1
+            B = diag(1 ./ diag(A));
+        case 2
+            % SGS
+        case 3
+            % SSOR
+        otherwise
+            B = eye(size(A,1));
+    end
+    b_norm_tol = norm(b) * tol;
+    
+    x_crnt = x0;
+    r_crnt = b - A * x_crnt;
+    z_crnt = B * r_crnt;
+    p_crnt = z_crnt;
+    r_z_crnt = dot(r_crnt, z_crnt);
+
+    for k = 1:maxit      
+        m = m + 1;
+        
+        A_p_crnt = A * p_crnt;
+        ld = r_z_crnt / dot(p_crnt, A_p_crnt); 
+        x_next = x_crnt + ld * p_crnt;
+        r_next = r_crnt - ld * A_p_crnt;
+
+        if (norm(r_next) < b_norm_tol)
+            x = x_next;
+            return
+        end
+
+        z_next = B * r_next;
+        r_z_next = dot(r_next, z_next);
+        p_crnt = z_next + r_z_next / r_z_crnt * p_crnt;
+
+        x_crnt = x_next;
+        r_crnt = r_next;
+        r_z_crnt = r_z_next;
+    end
+	exception = MException('solvePCG reached maxit iterations. The iteration probably does not converge');
+	throw(exception);
+end
+
